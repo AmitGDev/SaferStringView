f62d2910288e0309ddbdc437ed0c9da30f1baff4
amitg <e_zi@hotmail.com>
2025-06-11 14:19:46 +0300

    Add project files.

---
+++ SaferStringView/SaferStringView.hpp
@@ -0,0 +1,73 @@
+#ifndef AMITG_FC_SAFERSTRINGVIEW
+#define AMITG_FC_SAFERSTRINGVIEW
+
+/*
+    SaferStringView.hpp
+    Copyright (c) 2025, Amit Gefen
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <variant>
+
+/**
+ * @brief String view that owns rvalues, views lvalues.
+ *
+ * Prevents dangling references by taking ownership of temporary strings.
+ * Lvalue strings must outlive the SaferStringView.
+ *
+ * @warning Cannot detect temporaries passed via string_view:
+ *          SaferStringView(std::string_view(std::to_string(2025))) // Dangling!
+ */
+template <typename T>
+  requires requires { typename std::char_traits<T>; }
+class SaferStringView final {
+ public:
+  // Non-owning: from lvalue string reference (stores view)
+  explicit SaferStringView(const std::basic_string<T> &str)
+      : storage_(std::basic_string_view<T>(str)) {}
+
+  // Owning: from rvalue string (stores owned string)
+  explicit SaferStringView(std::basic_string<T> &&str)
+      : storage_(std::move(str)) {}
+
+  // Non-owning: from string_view
+  explicit SaferStringView(std::basic_string_view<T> sv) : storage_(sv) {}
+
+  // Non-owning: from const char* (common use case)
+  explicit SaferStringView(const T *str)
+      : storage_(std::basic_string_view<T>(str)) {}
+
+  // Implicit conversion for drop-in string_view replacement
+  operator std::basic_string_view<T>() const {
+    return std::visit(
+        [](const auto &s) -> std::basic_string_view<T> { return s; }, storage_);
+  }
+
+ private:
+#ifdef _TEST
+ public:
+#endif
+  std::variant<std::basic_string<T>, std::basic_string_view<T>> storage_;
+};
+
+#endif
